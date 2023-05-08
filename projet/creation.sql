67c864e0161632f5d33b6316ab22bd5b402653b8
JustRayCB <rayancbpro@gmail.com>
2023-05-08 20:28:58 +0200

    All the table are correctly (normally) created

--- projet/creation.sql
+++ projet/creation.sql
@@ -14,22 +14,80 @@
  TODO: Table Dossier
 */
 
+-- Drop all tables if exists 
+-- DROP TABLE IF EXISTS DossierContientPathologie;
+-- DROP TABLE IF EXISTS PharmacienDelivreMedicament;
+-- DROP TABLE IF EXISTS MedecinPrescritPrescription;
+-- DROP TABLE IF EXISTS Prescription;
+-- DROP TABLE IF EXISTS Dossier;
+-- DROP TABLE IF EXISTS Medecin;
+-- DROP TABLE IF EXISTS Pharmacien;
+-- DROP TABLE IF EXISTS Pathologie;
+-- DROP TABLE IF EXISTS Specialisation;
+-- DROP TABLE IF EXISTS Medicament;
+-- DROP TABLE IF EXISTS SystemeAnatomique;
+
+
 -- WARNING: Un patient peut avoir plusieurs DOSSIERS, 
 --          mais un seul DOSSIER peut être lié à un patient
 /*
  WARNING: Je vais supprimer la table Patient et mettre tous dans la table dossier
 */
-CREATE DATABASE projetINFOH303_2223 if not exists;
+
+-- CREATE DATABASE [IF NOT EXISTS] database_name
+-- CREATE DATABASE projetINFOH303_2223 if not exists;
+create database if not exists projetINFOH303_2223;
 USE projetINFOH303_2223;
 
-/* Création de la table Patient 
- NOTE: Patient à besoin de Medecin, pharmacien et dossier
-*/ 
 
-/*
- WARNING: La table Patient et la Tale Dossier sont fusionnées (dossier n'existait pas avant)
- En Effet, il y a une relation 1, 1. Et j'ai décidé de mettre les infos du patient dans le dossier
-*/
+CREATE TABLE SystemeAnatomique (
+    Nom VARCHAR(50) NOT NULL PRIMARY KEY
+);
+
+CREATE TABLE Medicament(
+    ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    -- DCI VARCHAR(50) NOT NULL,
+    Nom VARCHAR(50) NOT NULL,
+    Conditionnement INT NOT NULL
+);
+
+-- NOTE: Spécialisation à besoin de SystemeAnatomique
+CREATE TABLE Specialisation (
+    Nom VARCHAR(50) NOT NULL PRIMARY KEY,
+    Systeme VARCHAR(50) DEFAULT NULL,
+    FOREIGN KEY(Systeme) REFERENCES SystemeAnatomique(Nom) ON DELETE SET NULL
+);
+
+-- SHOW TABLES;
+
+-- NOTE: Pathologie à besoin de Spécialisation
+CREATE TABLE Pathologie (
+    Nom VARCHAR(50) NOT NULL PRIMARY KEY,
+    SpecialisationNom VARCHAR(50),
+    Systeme VARCHAR(50),
+    FOREIGN KEY (SpecialisationNom) REFERENCES Specialisation(Nom) ON DELETE SET NULL,
+    FOREIGN KEY (Systeme) REFERENCES SystemeAnatomique(Nom) ON DELETE SET NULL
+);
+
+CREATE TABLE Pharmacien (
+    INAMI INT NOT NULL PRIMARY KEY,
+    Nom VARCHAR(50) NOT NULL,
+    NumTel INT NOT NULL,
+    Mail VARCHAR(50) DEFAULT NULL
+);
+
+-- NOTE: Médecin à besoin de Spécialisation 
+CREATE TABLE Medecin (
+    INAMI INT NOT NULL PRIMARY KEY,
+    Nom VARCHAR(50) NOT NULL,
+    NumTel INT NOT NULL,
+    Mail VARCHAR(50) DEFAULT NULL,
+    SpecialisationNom VARCHAR(50) NOT NULL,
+    FOREIGN KEY (SpecialisationNom) REFERENCES Specialisation(Nom)
+);
+
+
+-- NOTE: Dossier à besoin de Medecin et pharmacien
 CREATE TABLE Dossier (
     ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     Niss INT NOT NULL,
@@ -41,43 +99,11 @@ CREATE TABLE Dossier (
     NumTel INT DEFAULT NULL,
     PharmacienINAMI INT NOT NULL,
     MedecinINAMI INT NOT NULL,
-    FOREIGN KEY (PharmacienINAMI) REFERENCES Pharmacien(INAMI) ON DELETE SET NULL,
-    FOREIGN KEY (MedecinINAMI) REFERENCES Medecin(INAMI) ON DELETE SET NULL,
+    FOREIGN KEY (PharmacienINAMI) REFERENCES Pharmacien(INAMI),
+    FOREIGN KEY (MedecinINAMI) REFERENCES Medecin(INAMI)
 );
 
-/* Création de la table Pharmacien */ 
-CREATE TABLE Pharmacien (
-    INAMI INT NOT NULL PRIMARY KEY,
-    Nom VARCHAR(50) NOT NULL,
-    NumTel INT NOT NULL,
-    Mail VARCHAR(50) DEFAULT NULL
-);
-
-/* 
-Création de la table Médecin 
- NOTE: Médecin à besoin de Spécialisation 
- */
-CREATE TABLE Medecin (
-    INAMI INT NOT NULL PRIMARY KEY,
-    Nom VARCHAR(50) NOT NULL,
-    NumTel INT NOT NULL,
-    Mail VARCHAR(50) DEFAULT NULL,
-    SpecialisationNom INT NOT NULL,
-    FOREIGN KEY (SpecialisationNom) REFERENCES Specialisation(Nom) ON DELETE SET NULL
-);
-
-/* Création de la table Spécialisation */ 
-CREATE TABLE Specialisation (
-    Nom VARCHAR(50) NOT NULL PRIMARY KEY,
-    Systeme VARCHAR(50) DEFAULT NULL,
-    FOREIGN KEY(Systeme) REFERENCES SystemeAnatomique(Nom) ON DELETE SET NULL
-);
-
-/* 
-Création de la table Prescription 
-    NOTE: Prescription à besoin de Traitement et Médecin
-    On a merge Prescription et Traitement
- */
+-- NOTE: Prescription à besoin de Médecin, Medicament et Dossier
 CREATE TABLE Prescription (
     ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     MedecinINAMI INT DEFAULT NULL,
@@ -88,16 +114,14 @@ CREATE TABLE Prescription (
     MedicamentID INT DEFAULT NULL, -- Celui choisi par le pharmacien
     FOREIGN KEY (MedecinINAMI) REFERENCES Medecin(INAMI) ON DELETE SET NULL,
     FOREIGN KEY (DossierID) REFERENCES Dossier(ID) ON DELETE CASCADE,
-    FOREIGN KEY(MedicamentID) REFERENCES Medicament(ID) ON DELETE SET NULL,
+    FOREIGN KEY(MedicamentID) REFERENCES Medicament(ID) ON DELETE SET NULL
     -- FOREIGN KEY (MedicamentNom) REFERENCES Medicament(Nom) ON DELETE SET NULL
     -- FOREIGN KEY (MedecinNom) REFERENCES Medecin(Nom) ON DELETE SET NULL,
     -- FOREIGN KEY (PharmacienINAMI) REFERENCES Pharmacien(INAMI) ON DELETE SET NULL,
 );
 
-/*
-Création de la table MédecinPrescritPrescription
-    NOTE: MédecinPrescritPrescription à besoin de Médecin et Prescription
-*/
+
+-- NOTE: MédecinPrescritPrescription à besoin de Médecin et Prescription
 CREATE TABLE MedecinPrescritPrescription (
     MedecinINAMI INT NOT NULL,
     PrescriptionID INT NOT NULL,
@@ -108,55 +132,25 @@ CREATE TABLE MedecinPrescritPrescription (
     PRIMARY KEY (MedecinINAMI, PrescriptionID)
 );
 
-/*
-Création de la table Médicament
-*/
-CREATE TABLE Medicament(
-    ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    DCI VARCHAR(50) NOT NULL,
-    Nom VARCHAR(50) NOT NULL,
-    Conditionnement INT NOT NULL,
-);
 
-/*
-Création de la table PharmacienDélivreMédicament
-    NOTE: PharmacienDélivreMédicament à besoin de Pharmacien et Médicament
-*/
+-- NOTE: PharmacienDélivreMédicament à besoin de Pharmacien et Médicament
 CREATE TABLE PharmacienDelivreMedicament(
     PharmacienINAMI INT NOT NULL,
-    MedicamentDCI VARCHAR(50) NOT NULL,
-    FOREIGN KEY (PharmacieINAMI) REFERENCES Pharmacie(INAMI) ON DELETE CASCADE,
-    FOREIGN KEY (MedicamentDCI) REFERENCES Medicament(DCI) ON DELETE CASCADE,
-    PRIMARY KEY (PharmacieINAMI, MedicamentDCI)
+    MedicamentID INT NOT NULL,
+    FOREIGN KEY (PharmacienINAMI) REFERENCES Pharmacien(INAMI) ON DELETE CASCADE,
+    FOREIGN KEY (MedicamentID) REFERENCES Medicament(ID) ON DELETE CASCADE,
+    PRIMARY KEY (PharmacienINAMI, MedicamentID)
 );
 
-/*
-Création de la table Dossier
-    NOTE: Dossier à besoin Dossier et de Pathologie
-*/
+
+-- NOTE: Dossier à besoin Dossier et de Pathologie
 CREATE TABLE DossierContientPathologie (
-    DID INT NOT NULL PRIMARY KEY,
-    PathologieNom INT NOT NULL,
+    DID INT NOT NULL,
+    PathologieNom VARCHAR(50) NOT NULL,
     DateDiagnostique DATE NOT NULL,
     FOREIGN KEY (DID) REFERENCES Dossier(ID) ON DELETE CASCADE,
-    FOREIGN KEY (PathologieNom) REFERENCES Pathologie(Nom) ON SET NULL,
-    PRIMARY KEY (DID, PathologieID)
-);
-
-/*
-Création de la table Pathologie 
-    NOTE: Pathologie à besoin de Spécialisation
-*/
-CREATE TABLE Pathologie (
-    Nom VARCHAR(50) NOT NULL PRIMARY KEY,
-    Systeme VARCHAR(50) NOT NULL
-    SpecialisationNom VARCHAR(50) NOT NULL,
-    FOREIGN KEY (SpecialisationNom) REFERENCES Specialisation(Nom) ON DELETE SET NULL,
-    FOREIGN KEY (Systeme) REFERENCES SystemeAnatomique(Nom) ON DELETE SET NULL
-);
-
-CREATE TABLE SystemeAnatomique (
-    Nom VARCHAR(50) NOT NULL PRIMARY KEY,
+    FOREIGN KEY (PathologieNom) REFERENCES Pathologie(Nom) ON DELETE CASCADE,
+    PRIMARY KEY (DID, PathologieNom)
 );
 
 
