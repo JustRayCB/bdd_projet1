779f3f12c1d43e35407cb1f99acce226fd84ad38
JustRayCB <rayancbpro@gmail.com>
2023-05-09 22:16:09 +0200

    The script show the tables correctly

--- code/main.cpp
+++ code/main.cpp
@@ -1,4 +1,8 @@
+#include <cppconn/connection.h>
+#include <cppconn/prepared_statement.h>
+#include <cppconn/statement.h>
 #include <iostream>
+#include <memory>
 #include <mysql_connection.h>
 #include <mysql_driver.h>
 #include <mysql_error.h>
@@ -7,26 +11,39 @@ using namespace std;
 
 int main() {
 
-  sql::mysql::MySQL_Driver *driver;
-  sql::Connection *con;
+    sql::mysql::MySQL_Driver *driver;
+    sql::Connection *con;
 
-  driver = sql::mysql::get_mysql_driver_instance();
-  sql::ConnectOptionsMap connection_properties;
-  connection_properties["hostName"] =
-      "tcp://localhost:3306/projetINFOH303_2223";
-  connection_properties["userName"] = "root";
-  connection_properties["password"] = "root";
-  connection_properties["OPT_RECONNECT"] =
-      true; // Optionnel : Permet la reconnexion automatique en cas de
-            // déconnexion
-  con = driver->connect(connection_properties);
+    sql::ConnectOptionsMap connection_properties;
+    connection_properties["hostName"] = "tcp://localhost:3306/projetINFOH303_2223";
+    connection_properties["userName"] = "root";
+    connection_properties["password"] = "root";
+    connection_properties["OPT_RECONNECT"] = true; // Optionnel : Permet la reconnexion automatique
+                                                   // en cas de déconnexion
+    try {
+        driver = sql::mysql::get_mysql_driver_instance();
+        con = driver->connect(connection_properties);
+        std::unique_ptr<sql::Statement> stmt(con->createStatement());
+        // con->setSchema("projetINFOH303_2223");
+        std::unique_ptr<sql::ResultSet> res(stmt->executeQuery("SHOW TABLES"));
+        while (res->next()) {
+            cout << "\t... MySQL replies: ";
+            /* Access column data by alias or column name */
+            cout << res->getString("Tables_in_projetINFOH303_2223") << endl;
+        }
 
-  if (con->isValid()) {
-    cout << "Connection is valid" << endl;
-  } else {
-    cout << "Connection is invalid" << endl;
-  }
-  delete con;
+    } catch (sql::SQLException &e) {
+        cout << "# ERR: SQLException in " << __FILE__;
+        cout << "(" << __FUNCTION__ << ") on line " << __LINE__ << endl;
+        /* what() (derived from std::runtime_error) fetches error message */
+        cout << "# ERR: " << e.what();
+        cout << " (MySQL error code: " << e.getErrorCode();
+        cout << ", SQLState: " << e.getSQLState() << " )" << endl;
 
-  return 0;
+        return EXIT_FAILURE;
+    }
+
+    delete con;
+
+    return 0;
 }
