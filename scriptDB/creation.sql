8ad5a613d4e28fbadd28f1c10c377308dfc1cad1
JustRayCB <rayancbpro@gmail.com>
2023-05-09 15:32:19 +0200

    Nome of projet changed

--- scriptDB/creation.sql
+++ scriptDB/creation.sql
@@ -1,30 +1,5 @@
--- Drop all tables if exists 
--- DROP TABLE IF EXISTS DossierContientPathologie;
--- DROP TABLE IF EXISTS PharmacienDelivreMedicament;
--- DROP TABLE IF EXISTS MedecinPrescritPrescription;
--- DROP TABLE IF EXISTS Prescription;
--- DROP TABLE IF EXISTS Dossier;
--- DROP TABLE IF EXISTS Medecin;
--- DROP TABLE IF EXISTS Pharmacien;
--- DROP TABLE IF EXISTS Pathologie;
--- DROP TABLE IF EXISTS SpecialisationSpecialiseSysAnatomique;
--- DROP TABLE IF EXISTS Specialisation;
--- DROP TABLE IF EXISTS SystemeAnatomique;
--- DROP TABLE IF EXISTS Medicament;
--- ALTER TABLE SpecialisationSpecialiseSysA DROP FOREIGN KEY SpecialisationSpecialiseSysA_ibfk_1;
--- ALTER TABLE SpecialisationSpecialiseSysA DROP FOREIGN KEY SpecialisationSpecialiseSysA_ibfk_2;
--- ALTER TABLE Specialisation DROP FOREIGN KEY Specialisation_ibfk_1;
 
 -- SHOW TABLES;
-
--- WARNING: Un patient peut avoir plusieurs DOSSIERS, 
---          mais un seul DOSSIER peut être lié à un patient
-/*
- WARNING: Je vais supprimer la table Patient et mettre tous dans la table dossier
-*/
-
--- CREATE DATABASE [IF NOT EXISTS] database_name
--- CREATE DATABASE projetINFOH303_2223 if not exists;
 create database if not exists projetINFOH303_2223;
 USE projetINFOH303_2223;
 
