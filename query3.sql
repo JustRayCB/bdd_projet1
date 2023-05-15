9420b48036b62f68b7d52a326ff4b1a9aeacc746
Rogge Ethan G <93152548+Ethanrogge@users.noreply.github.com>
2023-05-15 19:04:00 +0200

    Delete query3.sql

--- query3.sql
+++
@@ -1,7 +0,0 @@
--- Active: 1683463812091@@127.0.0.1@3306@projetINFOH303_2223
-SELECT m.`SpecialisationNom`, COUNT(*) AS total_prescriptions
-FROM Medecin m
-JOIN Prescription p ON m.`INAMI` = p.`MedecinINAMI`
-GROUP BY m.`SpecialisationNom`
-ORDER BY total_prescriptions DESC
-LIMIT 1;
