b34f21b0472d3d4f991fd4cfff1979dd47319018
callhc <hugo.callens@ulb.be>
2023-05-15 09:44:54 +0200

    Adding queries 6 to 10 in SQL

---
+++ queries/query7.sql
@@ -0,0 +1,28 @@
+SELECT
+    CONCAT(Decade, '0') As Decade,
+    Medicament,
+    Nombre_Patients
+FROM (
+    --Sous-requête pour calculer le nombre de patients par décennie et par médicament
+    SELECT
+        --Déduction de la décennie par rapport à la date de naissance du patient
+        SUBSTRING(d.DateNaissance, 1, 3) AS Decade,
+        m.DCI AS Medicament,
+        COUNT(*) AS Nombre_Patients,
+        --Un numéro de ligne par décennie en fonction du nombre de patients (tri descendant)
+        ROW_NUMBER() OVER (PARTITION BY SUBSTRING(d.DateNaissance, 1, 3) ORDER BY COUNT(*) DESC) AS RowNum
+    FROM
+        Dossier AS d
+        JOIN Prescription AS p ON d.Niss = p.DossierID
+        JOIN Medicament AS m ON p.MedicamentNom = m.Nom
+    WHERE
+        --Filtre pour inclure uniquement les patients nés entre 1950 et 2000
+        SUBSTRING(d.DateNaissance, 1, 3) BETWEEN '195' AND '200'
+    GROUP BY
+        Decade,
+        Medicament
+) AS T
+WHERE
+    RowNum = 1
+ORDER BY
+    Decade;
