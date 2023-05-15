ae41b659d4e1360b21500c385833df7f820f2344
Rogge Ethan G <93152548+Ethanrogge@users.noreply.github.com>
2023-05-15 19:04:09 +0200

    Delete query4.sql

--- query4.sql
+++
@@ -1,7 +0,0 @@
--- Active: 1683463812091@@127.0.0.1@3306@projetINFOH303_2223
-SELECT DISTINCT d.`Niss`, d.`Nom`, d.`Prenom`, d.`Mail`, d.`NumTel`
-FROM Dossier d
-JOIN Prescription p ON d.`Niss` = p.`DossierID`
-JOIN PharmacienDelivreMedicament pdm ON p.`ID` = pdm.`PrescriptionID`
-WHERE p.`MedicamentNom` = 'Lisinopril' 
-AND pdm.`DateDelivrance` > '1947-04-09';
