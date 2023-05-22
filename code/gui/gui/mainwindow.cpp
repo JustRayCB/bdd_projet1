f527667466932062cc0048fa3430ef2a88f94c08
JustRayCB <rayancbpro@gmail.com>
2023-05-22 13:41:43 +0200

    Fix problem that cannot load project

--- code/gui/gui/mainwindow.cpp
+++ code/gui/gui/mainwindow.cpp
@@ -1,4 +1,4 @@
-#include "mainwindow.h"
+#include "mainwindow.hpp"
 #include "./ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
@@ -11,23 +11,5 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow()
 {
     delete ui;
-}
-
-
-void MainWindow::on_pushButton_clicked()
-{
-    QSqlDatabase db= QSqlDatabase::addDatabase("QMYSQL");
-        db.setHostName("127.0.0.1");
-        db.setUserName("root");
-        db.setPassword("");
-        db.setDatabaseName("hospital");
-
-        if(db.open()){
-            QMessageBox::information(this, "Connection", "Database connected Succes");
-
-        } else {
-            QMessageBox::information(this,"Not Connected","Database is nnot connected");
-
-        }
 }
 
