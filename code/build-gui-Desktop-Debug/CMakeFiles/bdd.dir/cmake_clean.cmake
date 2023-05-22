c56eb4410dc7eaf812574c3b703509fc1b964c7e
Ethanrogge <roggeethan2003@gmail.com>
2023-05-22 10:48:15 +0200

    ajout gui

---
+++ code/build-gui-Desktop-Debug/CMakeFiles/bdd.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bdd_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/bdd_autogen.dir/ParseCache.txt"
+  "bdd_autogen"
+  "CMakeFiles/bdd.dir/bdd_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/bdd.dir/bdd_autogen/mocs_compilation.cpp.o.d"
+  "CMakeFiles/bdd.dir/main.cpp.o"
+  "CMakeFiles/bdd.dir/main.cpp.o.d"
+  "CMakeFiles/bdd.dir/mainwindow.cpp.o"
+  "CMakeFiles/bdd.dir/mainwindow.cpp.o.d"
+  "bdd"
+  "bdd.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bdd.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ code/build-gui-Desktop-Debug/CMakeFiles/bdd_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bdd_autogen"
+  "bdd_autogen/include/ui_mainwindow.h"
+  "bdd_autogen/mocs_compilation.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/bdd_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
