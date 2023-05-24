65f3ad1e113ab7c4d18603e48b431de6c1e2e7bc
callhc <hugo.callens@ulb.be>
2023-05-24 17:13:34 +0200

    Good answer query modified

--- scriptDB/query6.sql
+++ scriptDB/query6.sql
@@ -1,9 +1,14 @@
-SELECT DISTINCT M.Nom
+SELECT M.INAMI, M.Nom, M.NumTel, M.Mail, M.SpecialisationNom
 FROM Medecin M
-JOIN Prescription P ON M.INAMI = P.MedecinINAMI
-JOIN Medicament Med ON P.MedicamentNom = Med.Nom
+INNER JOIN Prescription P ON M.INAMI = P.MedecinINAMI
+INNER JOIN Medicament Med ON P.MedicamentNom = Med.Nom
+LEFT JOIN Specialisation S ON M.SpecialisationNom = S.Nom
+LEFT JOIN SpecialisationSpecialiseSysAnatomique SSA ON S.Nom = SSA.SpecialisationNom
+LEFT JOIN SystemeAnatomique SA ON SSA.SystemeAnatomiqueNom = SA.Nom
 WHERE Med.SystemeAnatomiqueNom NOT IN (
     SELECT SSA.SystemeAnatomiqueNom
     FROM SpecialisationSpecialiseSysAnatomique SSA
     WHERE SSA.SpecialisationNom = M.SpecialisationNom
-);
+)
+OR (Med.SystemeAnatomiqueNom IS NULL AND M.SpecialisationNom IS NOT NULL)
+GROUP BY M.INAMI, M.Nom, M.NumTel, M.Mail, M.SpecialisationNom;
