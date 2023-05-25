f03689055e12b8c5f1612e5211e8d624b08419d1
callhc <hugo.callens@ulb.be>
2023-05-25 20:32:58 +0200

    Rectifying query 6

--- scriptDB/query6.sql
+++ scriptDB/query6.sql
@@ -2,13 +2,10 @@ SELECT M.INAMI, M.Nom
 FROM Medecin M
 INNER JOIN Prescription P ON M.INAMI = P.MedecinINAMI
 INNER JOIN Medicament Med ON P.MedicamentNom = Med.Nom
-LEFT JOIN Specialisation S ON M.SpecialisationNom = S.Nom
-LEFT JOIN SpecialisationSpecialiseSysAnatomique SSA ON S.Nom = SSA.SpecialisationNom
-LEFT JOIN SystemeAnatomique SA ON SSA.SystemeAnatomiqueNom = SA.Nom
+LEFT JOIN SpecialisationSpecialiseSysAnatomique SSA ON M.SpecialisationNom = SSA.SpecialisationNom
 WHERE Med.SystemeAnatomiqueNom NOT IN (
     SELECT SSA.SystemeAnatomiqueNom
     FROM SpecialisationSpecialiseSysAnatomique SSA
     WHERE SSA.SpecialisationNom = M.SpecialisationNom
 )
-OR (Med.SystemeAnatomiqueNom IS NULL AND M.SpecialisationNom IS NOT NULL)
-GROUP BY M.INAMI, M.Nom, M.NumTel, M.Mail, M.SpecialisationNom;
+GROUP BY M.INAMI, M.Nom;
