51e785f074449b7b64937783019d22b8b6e392cc
callhc <hugo.callens@ulb.be>
2023-05-26 18:05:10 +0200

    Unifying INNER JOIN -> JOIN

--- scriptDB/query6.sql
+++ scriptDB/query6.sql
@@ -1,7 +1,7 @@
 SELECT M.INAMI, M.Nom
 FROM Medecin M
-INNER JOIN Prescription P ON M.INAMI = P.MedecinINAMI
-INNER JOIN Medicament Med ON P.MedicamentNom = Med.Nom
+JOIN Prescription P ON M.INAMI = P.MedecinINAMI
+JOIN Medicament Med ON P.MedicamentNom = Med.Nom
 LEFT JOIN SpecialisationSpecialiseSysAnatomique SSA ON M.SpecialisationNom = SSA.SpecialisationNom
 WHERE Med.SystemeAnatomiqueNom NOT IN (
     SELECT SSA.SystemeAnatomiqueNom
